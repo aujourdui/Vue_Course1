cd517a027533c05d80963017d4c8ff92d3151305
kyosuke <kyosuke.ito@gmail.com>
2022-03-17 17:42:07 -0700

    how to use method

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -5,6 +5,16 @@ const app = Vue.createApp({
       vueLink: "https://vuejs.org/guide/quick-start.html#without-build-tools",
     };
   },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return "Learn Vue!";
+      } else {
+        return "Master Vue!";
+      }
+    },
+  },
 });
 
 app.mount("#user-goal");
