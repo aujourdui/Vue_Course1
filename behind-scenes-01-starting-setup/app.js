1e8aa918b0f06df1d5dd24a528c14e255e976841
kyosuke <kyosuke.ito@gmail.com>
2022-03-20 12:16:49 -0700

    how to use lifecycle method in vue

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -8,14 +8,45 @@ const app = Vue.createApp({
   methods: {
     saveInput(event) {
       this.currentUserInput = event.target.value;
+      // console.dir(event.target);
     },
     setText() {
-      this.message = this.currentUserInput;
+      // this.message = this.currentUserInput;
+      this.message = this.$refs.userText.value;
+      // console.dir(this.$refs.userText); // console.dir === console whole property
     },
+  },
+  beforeCreate() {
+    console.log("beforeCreate()");
+  },
+  created() {
+    console.log("created()");
+  },
+  beforeMount() {
+    console.log("beforeMount()");
+  },
+  mounted() {
+    console.log("mounted()");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate()");
+  },
+  updated() {
+    console.log("updated()");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount()");
+  },
+  unmounted() {
+    console.log("unmounted()");
   },
 });
 
 app.mount("#app");
+
+setTimeout(() => {
+  app.unmount();
+}, 3000);
 
 const app2 = Vue.createApp({
   template: `
