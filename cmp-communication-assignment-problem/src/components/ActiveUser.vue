7cc14e209277f718c59506aa9acecbf09f7ed023
kyosuke <kyosuke.ito@gmail.com>
2022-03-21 15:24:08 -0700

    finish assigmment about component

---
+++ cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2>{{ username }}</h2>
+  <h3>{{ age }}</h3>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    // return {
+    //   username: "kyosuke",
+    //   age: "31",
+    // };
+  },
+};
+</script>
---
+++ cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,26 @@
+<template>
+  <form @submit.prevent="submitData">
+    <label for="username">username</label>
+    <input type="text" v-model="enteredUsername" />
+    <label for="age">age</label>
+    <input type="number" v-model="enteredAge" />
+    <button>Set user data</button>
+  </form>
+</template>
+
+<script>
+export default {
+  emit: ["pass-userdata"],
+  data() {
+    return {
+      enteredUsername: "",
+      enteredAge: "",
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit("pass-userdata", this.enteredUsername, this.enteredAge);
+    },
+  },
+};
+</script>
