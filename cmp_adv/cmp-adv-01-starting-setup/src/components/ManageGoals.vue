579b6cc3c31460bf422e901bc68fc47e132bda01
kyosuke <kyosuke.ito@gmail.com>
2022-03-21 23:48:42 -0700

    styleguide and other features

--- cmp_adv/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp_adv/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -25,16 +25,14 @@ export default {
 </script>
 
 <template>
-  <div>
-    <h2>Manage Goals</h2>
-    <input type="text" ref="goal" />
-    <button @click="setGoal">Set Goal</button>
-    <Teleport to="body">
-      <ErrorAlert v-if="inputIsInvalid">
-        <h2>Input is invalid</h2>
-        <p>Please enter at least a few characters...</p>
-        <button @click="confirmError">Okay</button>
-      </ErrorAlert>
-    </Teleport>
-  </div>
+  <h2>Manage Goals</h2>
+  <input type="text" ref="goal" />
+  <button @click="setGoal">Set Goal</button>
+  <Teleport to="body">
+    <ErrorAlert v-if="inputIsInvalid">
+      <h2>Input is invalid</h2>
+      <p>Please enter at least a few characters...</p>
+      <button @click="confirmError">Okay</button>
+    </ErrorAlert>
+  </Teleport>
 </template>
