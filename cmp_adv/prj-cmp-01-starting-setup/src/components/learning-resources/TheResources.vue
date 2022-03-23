2d021ba696e3723aaa802f978adb84d2cb14962c
kyosuke <kyosuke.ito@gmail.com>
2022-03-22 21:19:14 -0700

    add style when change the tab

--- cmp_adv/prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
+++ cmp_adv/prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
@@ -1,31 +1,62 @@
 <script>
-import StoredResources from "./StoredResources"
-import AddResource from "./AddResource"
+import StoredResources from './StoredResources';
+import AddResource from './AddResource';
+// import { computed } from 'vue';
 
 export default {
-  components:{
+  components: {
     StoredResources,
-    AddResource
+    AddResource,
   },
   data() {
     return {
-      selectTab: 'stored-resources',
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com',
+        },
+      ],
     };
   },
-  methods {
+  provide() {
+    return {
+      // resources: (() => this.storedResources),
+      resources: this.storedResources,
+    };
+  },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
+    },
+    addResButtonMode() {
+      return this.selectedTab === 'add-resource' ? null : 'flat';
+    },
+  },
+  methods: {
     setSelectedTab(tab) {
-      this.selectedTab = tab
-    }
-  }
+      this.selectedTab = tab;
+    },
+  },
 };
 </script>
 
 <template>
   <BaseCard>
-    <BaseButton @click="setSelectedTab('stored-resources')"
+    <BaseButton
+      @click="setSelectedTab('stored-resources')"
+      :mode="storedResButtonMode"
       >Stored Resources</BaseButton
     >
-    <BaseButton @click="setSelectedTab('add-resource')"
+    <BaseButton @click="setSelectedTab('add-resource')" :mode="addResButtonMode"
       >Add Resource</BaseButton
     >
   </BaseCard>
