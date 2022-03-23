c288328bd4c4f316aa97c072cf01908cd9698136
kyosuke <kyosuke.ito@gmail.com>
2022-03-22 19:55:14 -0700

    create a base button

--- cmp_adv/prj-cmp-01-starting-setup/src/components/learning-resources/LearningResource.vue
+++ cmp_adv/prj-cmp-01-starting-setup/src/components/learning-resources/LearningResource.vue
@@ -9,7 +9,7 @@ export default {
     <BaseCard>
       <header>
         <h3>{{ title }}</h3>
-        <button>Delete</button>
+        <BaseButton mode="flat">Delete</BaseButton>
       </header>
       <p>{{ description }}</p>
       <nav>
