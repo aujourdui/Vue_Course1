21615ac35231404ad8536affc06ce48b04cc1b9d
kyosuke <kyosuke.ito@gmail.com>
2022-03-23 16:28:40 -0700

    add vue router

--- intermediate/routing-01-starting-setup/src/App.vue
+++ intermediate/routing-01-starting-setup/src/App.vue
@@ -1,24 +1,12 @@
-<template>
-  <the-navigation @set-page="setActivePage"></the-navigation>
-  <main>
-    <component :is="activePage"></component>
-  </main>
-</template>
-
 <script>
-import TeamsList from './components/teams/TeamsList.vue';
-import UsersList from './components/users/UsersList.vue';
 import TheNavigation from './components/nav/TheNavigation.vue';
 
 export default {
   components: {
     TheNavigation,
-    TeamsList,
-    UsersList,
   },
   data() {
     return {
-      activePage: 'teams-list',
       teams: [
         { id: 't1', name: 'Frontend Engineers', members: ['u1', 'u2'] },
         { id: 't2', name: 'Backend Engineers', members: ['u1', 'u2', 'u3'] },
@@ -39,13 +27,15 @@ export default {
       users: this.users,
     };
   },
-  methods: {
-    setActivePage(page) {
-      this.activePage = page;
-    },
-  },
 };
 </script>
+
+<template>
+  <the-navigation></the-navigation>
+  <main>
+    <RouterView />
+  </main>
+</template>
 
 <style>
 * {
