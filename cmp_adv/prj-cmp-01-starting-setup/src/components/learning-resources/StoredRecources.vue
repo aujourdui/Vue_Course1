2e5a22affe11a06152027beedf882f37446aa4a3
kyosuke <kyosuke.ito@gmail.com>
2022-03-22 17:41:08 -0700

    try to add storedResources

---
+++ cmp_adv/prj-cmp-01-starting-setup/src/components/learning-resources/StoredRecources.vue
@@ -0,0 +1,22 @@
+<script>
+import LearningResource from './LearningResource.vue';
+
+export default {
+  props: ['resources'],
+  components: {
+    LearningResource,
+  },
+};
+</script>
+
+<template>
+  <ul>
+    <LearningResource
+      v-for="res in resources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    />
+  </ul>
+</template>
