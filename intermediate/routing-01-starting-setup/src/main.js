538f74d305ed859732ffa418215ae21ee7a48269
kyosuke <kyosuke.ito@gmail.com>
2022-03-24 12:29:14 -0700

    how to add nested route efficiently

--- intermediate/routing-01-starting-setup/src/main.js
+++ intermediate/routing-01-starting-setup/src/main.js
@@ -12,9 +12,17 @@ const router = createRouter({
   routes: [
     { path: '/', redirect: '/teams' },
     {
+      name: 'teams',
       path: '/teams',
       component: TeamsList,
-      children: [{ path: ':teamId', component: TeamMembers, props: true }], // /teams/t1
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true,
+        },
+      ], // /teams/t1
     },
     // { path: '/teams', component: TeamsList, alias: '/' },
     { path: '/users', component: UsersList },
