543eabba3e425c1c7a8d5938c905b672e0a56f75
kyosuke <kyosuke.ito@gmail.com>
2022-03-24 18:47:01 -0700

    how to use vue transion, particulary to control to desapper DOM

--- intermediate/animations-01-starting-setup/src/App.vue
+++ intermediate/animations-01-starting-setup/src/App.vue
@@ -4,6 +4,7 @@ export default {
     return {
       animatedBlock: false,
       dialogIsVisible: false,
+      paraIsVisible: false,
     };
   },
   methods: {
@@ -16,6 +17,9 @@ export default {
     animateBlock() {
       this.animatedBlock = true;
     },
+    toggleParagraph() {
+      this.paraIsVisible = !this.paraIsVisible;
+    },
   },
 };
 </script>
@@ -24,6 +28,12 @@ export default {
   <div class="container">
     <div :class="{ animate: animatedBlock }" class="block"></div>
     <button @click="animateBlock">Animate</button>
+  </div>
+  <div class="container">
+    <Transition>
+      <p v-if="paraIsVisible">This is only sometimes visible...</p>
+    </Transition>
+    <button @click="toggleParagraph">Toggle Paragraph</button>
   </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
@@ -81,7 +91,36 @@ button:active {
   animation: slide-fade 0.3s ease-out forwards;
 }
 
-@keyframes slide-fade {
+.v-enter-from {
+  /* opacity: 0;
+  transform: translateY(-30px); */
+}
+.v-enter-active {
+  /* transition: all 0.3s ease-out;
+   */
+  animation: slide-scale 0.3s ease-out;
+}
+.v-enter-to {
+  /* opacity: 1;
+  transform: translateY(0); */
+}
+
+.v-leave-from {
+  /* opacity: 1;
+  transform: translateY(0); */
+}
+
+.v-leave-active {
+  /* transition: all 0.3s ease-in;
+  */
+  animation: slide-scale 0.3s ease-out;
+}
+
+.v-leave-to {
+  /* opacity: 0;
+  transform: translateY(30px); */
+}
+@keyframes slide-scale {
   0% {
     transform: translateX(0) scale(1);
   }
