68b22ef5ede869e12c55ce577b3357e0178d3a94
kyosuke <kyosuke.ito@gmail.com>
2022-03-25 19:59:12 -0700

    add mutations and counter methods

--- intermediate/vuex-01-starting-setup/src/App.vue
+++ intermediate/vuex-01-starting-setup/src/App.vue
@@ -1,25 +1,29 @@
-<template>
-  <base-container title="Vuex">
-    <button @click="addOne">Add 1</button>
-  </base-container>
-</template>
-
 <script>
 import BaseContainer from './components/BaseContainer.vue';
 import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter';
 
 export default {
   components: {
     BaseContainer,
     TheCounter,
+    ChangeCounter,
   },
   methods: {
     addOne() {
-      this.$store.state.counter++;
+      this.$store.commit('increment');
     },
   },
 };
 </script>
+
+<template>
+  <base-container title="Vuex">
+    <TheCounter />
+    <button @click="addOne">Add 1</button>
+    <ChangeCounter />
+  </base-container>
+</template>
 
 <style>
 * {
---
+++ intermediate/vuex-01-starting-setup/src/components/ChangeCounter.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  methods: {
+    addOne() {
+      this.$store.commit('increment');
+    },
+  },
+};
+</script>
+
+<template>
+  <button @click="addOne">Add 1</button>
+</template>
