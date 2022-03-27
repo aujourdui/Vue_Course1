afa7da0f74929f4c3a3875069a20632ace5ba216
kyosuke <kyosuke.ito@gmail.com>
2022-03-26 23:45:03 -0700

    implement detail page

--- pj/main-prj-01-starting-setup/src/pages/coaches/CoachDetail.vue
+++ pj/main-prj-01-starting-setup/src/pages/coaches/CoachDetail.vue
@@ -1,5 +1,56 @@
+<script>
+export default {
+  props: ['id'],
+  data() {
+    return {
+      selectedCoach: null,
+    };
+  },
+  created() {
+    this.selectedCoach = this.$store.getters['coaches/coaches'].find(
+      (coach) => coach.id === this.id
+    );
+  },
+  computed: {
+    fullName() {
+      return this.selectedCoach.firstName + ' ' + this.selectedCoach.lastName;
+    },
+    areas() {
+      return this.selectedCoach.areas;
+    },
+    rate() {
+      return this.selectedCoach.hourlyRate;
+    },
+    description() {
+      return this.selectedCoach.description;
+    },
+    contactLink() {
+      return this.$route.path + '/' + this.id + '/contact';
+    },
+  },
+};
+</script>
+
 <template>
-  DETAILS FOR COACH
-  <RouterView />
-  <RouterLink to="/coaches/c1/contact">Contact</RouterLink>
+  <section>
+    <BaseCard>
+      <h2>{{ fullName }}</h2>
+      <h3>${{ rate }}/hour</h3>
+    </BaseCard>
+  </section>
+  <section>
+    <BaseCard>
+      <header>
+        <h2>Interested? Reach out now!</h2>
+        <BaseButton link :to="contactLink">Contact</BaseButton>
+      </header>
+      <RouterView></RouterView>
+    </BaseCard>
+  </section>
+  <section>
+    <BaseCard>
+      <BaseBadge v-for="area in areas" :key="area" :type="area" :title="area" />
+      <p>{{ description }}</p>
+    </BaseCard>
+  </section>
 </template>
