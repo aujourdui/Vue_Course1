b5caabd6eaeffa52abd23dfc03f15e94f504e1d0
kyosuke <kyosuke.ito@gmail.com>
2022-03-26 17:43:14 -0700

    add damy data and set getters to get list, and set hasCoaches method in case there is no coach

--- pj/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ pj/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -1,3 +1,16 @@
+<script>
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+};
+</script>
+
 <template>
   <section>FILTER</section>
   <section>
@@ -5,8 +18,11 @@
       <button>Refresh</button>
       <RouterLink to="./register">Register as Coach</RouterLink>
     </div>
-    <ul>
-      LIST OF COACHES
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.firstName }}
+      </li>
     </ul>
+    <h3 v-else>No coaches found</h3>
   </section>
 </template>
