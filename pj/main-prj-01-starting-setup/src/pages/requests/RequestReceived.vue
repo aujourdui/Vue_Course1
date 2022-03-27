28cb40afe87018a9c63d7a22149b5e69aec26cb2
kyosuke <kyosuke.ito@gmail.com>
2022-03-27 13:07:33 -0700

    send a request form and fix error which forgot return inside computed property

--- pj/main-prj-01-starting-setup/src/pages/requests/RequestReceived.vue
+++ pj/main-prj-01-starting-setup/src/pages/requests/RequestReceived.vue
@@ -1,1 +1,53 @@
-<template>REQUESTS</template>
+<script>
+import RequestItem from '../../components/requests/RequestItem';
+
+export default {
+  components: {
+    RequestItem,
+  },
+  computed: {
+    receivedRequests() {
+      return this.$store.getters['requests/requests'];
+    },
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests'];
+    },
+  },
+};
+</script>
+
+<template>
+  <section>
+    <BaseCard>
+      <header>
+        <h2>Request Received</h2>
+      </header>
+      <ul v-if="hasRequests">
+        <RequestItem
+          v-for="req in receivedRequests"
+          :key="req.id"
+          :email="req.userEmail"
+          :message="req.message"
+        />
+      </ul>
+      <h3 v-else>You haven't received any requests yet!</h3>
+    </BaseCard>
+  </section>
+</template>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
