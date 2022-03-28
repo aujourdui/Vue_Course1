a99f3df4f3fe431d514a221deeb843191d252a91
kyosuke <kyosuke.ito@gmail.com>
2022-03-28 11:14:15 -0700

    how to add nested route with composition

---
+++ composition/composition-17-routing-starting-project/src/pages/ProductDetails.vue
@@ -0,0 +1,43 @@
+<script>
+import { inject } from 'vue';
+
+export default {
+  props: ['pid'],
+  setup(props) {
+    const products = inject('products');
+
+    // const title = ref('');
+    // const price = ref(null);
+    // const description = ref('');
+
+    const selectedProduct = products.value.find(
+      (product) => product.id === props.pid
+    );
+
+    const title = selectedProduct.title;
+    const price = selectedProduct.price;
+    const description = selectedProduct.description;
+
+    return { title, price, description };
+  },
+};
+</script>
+
+<template>
+  <section>
+    <h2>{{ title }}</h2>
+    <h3>${{ price }}</h3>
+    <p>{{ description }}</p>
+    <RouterLink to="/products/p2">Product2</RouterLink>
+  </section>
+</template>
+
+<style scoped>
+section {
+  margin: 3rem auto;
+  max-width: 40rem;
+  padding: 1rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+}
+</style>
