1c7ab2aee203d8e4877afb2e72e7e3bb3c6b1b1c
kyosuke <kyosuke.ito@gmail.com>
2022-03-28 00:30:43 -0700

    how to pass props

--- composition/composition-01-starting-setup/src/components/UserData.vue
+++ composition/composition-01-starting-setup/src/components/UserData.vue
@@ -1,15 +1,28 @@
+<script>
+import { computed } from 'vue';
+
+export default {
+  props: ['firstName', 'lastName', 'age'],
+  setup(props) {
+    // const age = ref('age');
+    // const fName = re
+    const userName = computed(() => {
+      return props.firstName + ' ' + props.lastName;
+    });
+
+    return {
+      userName,
+    };
+  },
+  // computed: {
+  //   userName() {
+  //     return this.firstName + ' ' + this.lastName;
+  //   },
+  // },
+};
+</script>
+
 <template>
   <h2>{{ userName }}</h2>
   <h3>{{ age }}</h3>
 </template>
-
-<script>
-export default {
-  props: ['firstName', 'lastName', 'age'],
-  computed: {
-    userName() {
-      return this.firstName + ' ' + this.lastName;
-    },
-  },
-};
-</script>
