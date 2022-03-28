47a2d73aab4c952402848dcd1d37435e740122e9
kyosuke <kyosuke.ito@gmail.com>
2022-03-28 11:59:24 -0700

    how to use mixin

---
+++ composition/reuse-01-starting-setup/src/components/DeleteUser.vue
@@ -0,0 +1,21 @@
+<script>
+import UserAlert from '../components/UserAlert';
+import alertMixin from '../mixins/alert';
+
+export default {
+  components: {
+    UserAlert,
+  },
+  mixins: [alertMixin],
+};
+</script>
+
+<template>
+  <user-alert v-if="alertIsVisible" title="Delete the User?" @close="hideAlert">
+    <p>Do you want to continue with deleting a user?</p>
+  </user-alert>
+  <section>
+    <h2>Delete a User</h2>
+    <button @click="showAlert">Delete User</button>
+  </section>
+</template>
