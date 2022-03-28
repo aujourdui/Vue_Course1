0cf235cf895d3952da3190bad7949053b6a0c0e7
kyosuke <kyosuke.ito@gmail.com>
2022-03-27 21:23:26 -0700

    try to build and optimize folders

--- pj/main-prj-01-starting-setup/src/router.js
+++ pj/main-prj-01-starting-setup/src/router.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDetail from './pages/coaches/CoachDetail';
+// import CoachDetail from './pages/coaches/CoachDetail';
 import CoachesList from './pages/coaches/CoachesList';
-import CoachRegistration from './pages/coaches/CoachRegistration';
-import ContactCoach from './pages/requests/ContactCoach';
-import RequestReceived from './pages/requests/RequestReceived';
+// import CoachRegistration from './pages/coaches/CoachRegistration';
+// import ContactCoach from './pages/requests/ContactCoach';
+// import RequestReceived from './pages/requests/RequestReceived';
 import NotFound from './pages/NotFound';
+
+const CoachDetail = () => import('./pages/coaches/CoachDetail');
+const CoachRegistration = () => import('./pages/coaches/CoachRegistration');
+const ContactCoach = () => import('./pages/requests/ContactCoach');
+const RequestReceived = () => import('./pages/requests/RequestReceived');
 
 const router = createRouter({
   history: createWebHistory(),
