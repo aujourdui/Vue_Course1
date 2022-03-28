29ebef162344d8f84f95591de6f4714de00502f0
kyosuke <kyosuke.ito@gmail.com>
2022-03-27 18:59:26 -0700

    add function to use cache to optimize

--- pj/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ pj/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -48,10 +48,12 @@ export default {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
     },
-    async loadCoaches() {
+    async loadCoaches(refresh = false) {
       this.isLoading = true;
       try {
-        await this.$store.dispatch('coaches/loadCoaches');
+        await this.$store.dispatch('coaches/loadCoaches', {
+          forceRefresh: refresh,
+        });
       } catch (error) {
         this.error = error.message || 'Something went wrong!';
       }
@@ -72,7 +74,9 @@ export default {
   <section>
     <BaseCard>
       <div class="controls">
-        <BaseButton mode="outline" @click="loadCoaches">Refresh</BaseButton>
+        <BaseButton mode="outline" @click="loadCoaches(true)"
+          >Refresh</BaseButton
+        >
         <BaseButton v-if="!isCoach && !isLoading" link to="./register"
           >Register as Coach</BaseButton
         >
