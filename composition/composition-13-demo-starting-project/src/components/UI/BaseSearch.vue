d9c8c6e0e37f0c1a5a468c31ceddcaab235214f6
kyosuke <kyosuke.ito@gmail.com>
2022-03-28 11:01:29 -0700

    finish migrate project

---
+++ composition/composition-13-demo-starting-project/src/components/UI/BaseSearch.vue
@@ -0,0 +1,51 @@
+<script>
+// export default {
+//   props: ['searchTerm'],
+//   emits: ['search'],
+//   setup(context) {
+//     const search = (event) => {
+//       context.emit('search', event.target.value.value);
+//     };
+
+//     return {
+//       search,
+//     };
+//   },
+// };
+export default {
+  props: ['searchTerm'],
+  emits: ['search'],
+  methods: {
+    search(event) {
+      this.$emit('search', event.target.value);
+    },
+  },
+};
+</script>
+
+<template>
+  <form>
+    <input
+      type="search"
+      @input="search"
+      :value="searchTerm"
+      placeholder="Filter items"
+    />
+  </form>
+</template>
+
+<style scoped>
+input {
+  font: inherit;
+  width: 100%;
+  display: block;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus {
+  outline: none;
+  border-color: #00006b;
+  background-color: #eeeeff;
+}
+</style>
