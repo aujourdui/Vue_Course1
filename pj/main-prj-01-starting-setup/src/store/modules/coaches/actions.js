369719220fc116319fcbb66727c9cfdb62101099
kyosuke <kyosuke.ito@gmail.com>
2022-03-27 19:06:21 -0700

    add some animation for an error modal

--- pj/main-prj-01-starting-setup/src/store/modules/coaches/actions.js
+++ pj/main-prj-01-starting-setup/src/store/modules/coaches/actions.js
@@ -33,7 +33,7 @@ export default {
       return;
     }
     const response = await fetch(
-      `https://vue-coach-search-default-rtdb.firebaseio.com/coaches.json`
+      `https://vue-coach-search-default-rtdb.firebaseio.com/coaches.jso`
     );
     const responseData = await response.json();
 
