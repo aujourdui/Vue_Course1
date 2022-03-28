8c897eeea0c72bb3b327ad7f06d5f8056f802460
kyosuke <kyosuke.ito@gmail.com>
2022-03-27 19:18:34 -0700

    add transition to coach to request

--- pj/main-prj-01-starting-setup/src/pages/coaches/CoachDetail.vue
+++ pj/main-prj-01-starting-setup/src/pages/coaches/CoachDetail.vue
@@ -32,25 +32,32 @@ export default {
 </script>
 
 <template>
-  <section>
-    <BaseCard>
-      <h2>{{ fullName }}</h2>
-      <h3>${{ rate }}/hour</h3>
-    </BaseCard>
-  </section>
-  <section>
-    <BaseCard>
-      <header>
-        <h2>Interested? Reach out now!</h2>
-        <BaseButton link :to="contactLink">Contact</BaseButton>
-      </header>
-      <RouterView></RouterView>
-    </BaseCard>
-  </section>
-  <section>
-    <BaseCard>
-      <BaseBadge v-for="area in areas" :key="area" :type="area" :title="area" />
-      <p>{{ description }}</p>
-    </BaseCard>
-  </section>
+  <div>
+    <section>
+      <BaseCard>
+        <h2>{{ fullName }}</h2>
+        <h3>${{ rate }}/hour</h3>
+      </BaseCard>
+    </section>
+    <section>
+      <BaseCard>
+        <header>
+          <h2>Interested? Reach out now!</h2>
+          <BaseButton link :to="contactLink">Contact</BaseButton>
+        </header>
+        <RouterView></RouterView>
+      </BaseCard>
+    </section>
+    <section>
+      <BaseCard>
+        <BaseBadge
+          v-for="area in areas"
+          :key="area"
+          :type="area"
+          :title="area"
+        />
+        <p>{{ description }}</p>
+      </BaseCard>
+    </section>
+  </div>
 </template>
